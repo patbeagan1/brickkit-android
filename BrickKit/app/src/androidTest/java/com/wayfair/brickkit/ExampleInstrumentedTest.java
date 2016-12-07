7bf74a0cff0d8ce51d48f65fb7d90f3f56b67982
Tom Husson <thusson@wayfair.com>
2016-12-07 10:30:11 -0500

    Fix existing test / add coverage reporting

--- BrickKit/app/src/androidTest/java/com/wayfair/brickkit/ExampleInstrumentedTest.java
+++ BrickKit/app/src/androidTest/java/com/wayfair/brickkit/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.wayfair.brickkit", appContext.getPackageName());
+        assertEquals("com.wayfair.brickkitdemo", appContext.getPackageName());
     }
 }
